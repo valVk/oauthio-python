8a3249f07720245276149ab9d1e2baa1cd7760c5
Valeriy Ilihmetov <valeriy.ilihmetov@dev-pro.net>
2015-10-08 16:25:40 +0300

    Add empty structure

---
+++ oauthio-python/oauth.py
@@ -0,0 +1,5 @@
+__author__ = 'valeriy.ilihmetov'
+
+
+class Oauth:
+    pass
---
+++ setup.py
@@ -0,0 +1,16 @@
+__author__ = 'valeriy.ilihmetov'
+
+from distutils.core import setup
+
+setup(
+    name='oauthio-python',
+    packages=['oauthio-python'],
+    version='0.1',
+    description='OAuth.io backend authorisation',
+    author_email='ilikhmetov@gmail.com',
+    url='https://github.com',
+    download_url='https://github.com',
+    keywords=['oauthio', 'oauthd'],
+    classifiers=[],
+
+)
